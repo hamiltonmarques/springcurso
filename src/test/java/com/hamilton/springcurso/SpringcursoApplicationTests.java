9e718bd77ee56265c1374388fbd25c1b28f0aa55
Hamilton Marques <hamiltonwebpage@gmail.com>
2021-02-17 09:25:17 -0300

    initial commit

---
+++ src/test/java/com/hamilton/springcurso/SpringcursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamilton.springcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
